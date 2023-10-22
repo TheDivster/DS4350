a127601bfe2cd38c69bc8c38dabc2b53c5322560
divytripathy <divyt@protonmail.com>
2023-10-22 02:47:23 -0600

    Finished AdaBoost.py

--- Ensemble Learning/AdaBoost.py
+++ Ensemble Learning/AdaBoost.py
@@ -3,160 +3,67 @@ import typing
 import pandas as pd
 import numpy as np
 from typing import Callable
-from DecisionTreePackage.Node import Node
-
+from DecisionStump import DecisionStump
 
 
 class AdaBoost:
-  def __init__(self, data: pd.DataFrame) -> None:
-    self.__head = None
-    self.__data = data
-    self.__stumps: list[AdaBoost] = []
-    self.__alpha: list[float] = []
-
-  def build_boost(self, attributes: set, label: typing.Any, splitting_criteria: Callable, num_trees: int,
-                  depth: int = 2) -> None:
-    m: int = self.__data.shape[0]  # number of examples
-    weights: np.array = np.ones(shape=m) / m
-    for i in range(num_trees):
-      tree: AdaBoost = AdaBoost(self.__data)
-      tree.build(attributes, label, splitting_criteria, depth, weights)
-      epsilon_t: float = 0
-      for index in range(m):
-        if tree.__predict_single(self.__data, index) != (self.__data.iloc[index])[label]:
-          epsilon_t += weights[index]
-
-      alpha_t: float = 1 / 2 * math.log((1 - epsilon_t) / epsilon_t)
-      self.__alpha.append(alpha_t)
-      for index in range(m):
-        if tree.__predict_single(self.__data, index) == (self.__data.iloc[index])[label]:
-          weights[index] = weights[index] * np.exp(-alpha_t * 1)
-        else:
-          weights[index] = weights[index] * np.exp(-alpha_t * -1)
-      weights = weights / np.sum(weights)  # normalize to add weights to 1
-      self.__stumps.append(tree)
-
-  def _total_information_gain(self, data: pd.DataFrame, attributes: set, label: typing.Any, criteria: Callable,
-                              weights: np.array) -> str:
-    '''
-    The criteria is the function that we use to decide the "best" split. Each of its inputs should be a percent.
-    '''
-    data = data.copy()
-    data["weights"] = pd.Series(weights)
-    gain_dict: dict[float, str] = {}  # for quick retrival of attribute based on max information gain
-    gain_list: list[float] = []  # for quick max calculation
-    if criteria != self.majority_error:
-      proportions: list[float] = self.__fractional_proportions(data, label)  # TODO: Check this for changes
-      s_criteria = criteria(*proportions)
-    else:
-      s_criteria = criteria(data, label)
-    for attribute in attributes:
-      gain = s_criteria  # shortcut that stands for the entropy (or other splitting criteria) of s so, we don't
-      # have to recalculate s_criteria each iteration
-      for values in data[attribute].unique():
-        s_v: pd.DataFrame = data[data[attribute] == values]
-        if criteria != self.majority_error:
-          proportions: list[float] = self.__fractional_proportions(s_v, label)  # TODO: Check this for changes
-          criteria_s_v = criteria(*proportions)
-        else:
-          criteria_s_v = criteria(s_v, label)
-        gain -= (s_v.shape[0] / data.shape[0]) * criteria_s_v
-      gain_dict[gain] = attribute
-      gain_list.append(gain)
-    return gain_dict[max(gain_list)]
-
-  def __fractional_proportions(self, data: pd.DataFrame, label: str) -> list[float]:
-    '''
-    Calculate entropy based on every example being fractional
-    :param data: The section of data to calculate fractional proportions of
-    :param label: The label for what we are predicting
-    :return: A list of the proportions with all the proportions adding to one
-    '''
-    output: list = []
-    attribute_values: list = data[label].unique()
-    for attribute in attribute_values:
-      subset: pd.DataFrame = data[data[label] == attribute]
-      proportion: float = 0
-      for row in range(subset.shape[0]):  # loop over rows
-        proportion += (subset.iloc[row])['weights']  # add the weights
-        # TODO: Check if we need to normalize proportions
-      output.append(proportion)
-    return output
-
-
-  def _id3(self, data: pd.DataFrame, attributes: set, label: typing.Any, splitting_criteria: Callable, set_depth: int,
-           weights: np.array, depth: int = 0) -> Node:
     """
-        Implements id3 algorithm
-        Note: visualize is an experimental feature purely used for debugging
-        Assumes set_depth >= 0, but does not enforce this.
-        splitting criteria is the criteria used to define the "best" attribute to split on
-        Currently, criteria only works with self.entropy, self.ginni_index, self.majority_error
+    Used to build and predict AdaBoost models.
     """
-    # Base case
-    if len(data[label].unique()) == 1:
-      # Note: mode returns an array
-      return Node(data[label].mode()[0])  # all columns of the label should have the same attribute
-    if len(attributes) == 0 or depth >= set_depth:
-      most_common_value_node: Node = Node(data[label].mode()[0])
-      return most_common_value_node
+    def __init__(self, data: pd.DataFrame) -> None:
+        self.__data = data
+        self.__stumps: list[DecisionStump] = []
+        self.__alpha: list[float] = []
+        self.ENTROPY = DecisionStump.entropy
+        self.GINNI_INDEX = DecisionStump.ginni_index
+        self.MAJORITY_ERROR = DecisionStump.majority_error
 
-    a: typing.Any = self._total_information_gain(data, attributes, label, splitting_criteria, weights)
-    root: Node = Node(a)
-    for values in self.__data[a].unique():
-      s_v: pd.DataFrame = data[data[a] == values]
-      if s_v.shape[0] == 0:  # shape[0] gives the number of rows/observations
-        leaf_node: Node = Node(data[label].mode()[0])  # can't do this if s_v shape is empty
-        root.add_child(values, leaf_node)
-      else:
-        this_node: Node = self._id3(s_v, attributes - {a}, label, splitting_criteria, set_depth, depth + 1)
-        root.add_child(values, this_node)
-    return root
+    def build(self, attributes: set, label: typing.Any, splitting_criteria: Callable, num_trees: int,
+              depth: int = 2) -> None:
+        """
+        Implements AdaBoost algorithm on the bases of the DecisionStump class.
+        Note, A tree that will perfectly fit will give a zero division error
+        :param attributes: Features
+        :param label: What we are predicting
+        :param splitting_criteria: What function should we use to split
+        :param num_trees: The number of stumps we should use to make a prediction
+        :param depth: The max depth of the stumps
+        :return: void
+        """
+        m: int = self.__data.shape[0]  # number of examples
+        weights: np.array = np.ones(shape=m) / m
+        for i in range(num_trees):
+            tree: DecisionStump = DecisionStump(self.__data)
+            tree.build(attributes, label, splitting_criteria, weights, depth)
+            epsilon_t: float = 0
+            for index in range(m):
+                if tree.predict(self.__data, index) != (self.__data.iloc[index])[label]:
+                    epsilon_t += weights[index]
 
-  def build(self, attributes: set, label: typing.Any, splitting_criteria: Callable, set_depth: int, weights: np.array,
-            depth: int = 0) -> None:
-    '''
-    Builds and stores the decision tree\
-    '''
-    self.__head = self._id3(self.__data, attributes, label, splitting_criteria, set_depth, weights, depth)
+            alpha_t: float = 1 / 2 * math.log((1 - epsilon_t) / epsilon_t)
+            self.__alpha.append(alpha_t)
+            for index in range(m):
+                if tree.predict(self.__data, index) == (self.__data.iloc[index])[label]:
+                    weights[index] = weights[index] * np.exp(-alpha_t * 1)
+                else:
+                    weights[index] = weights[index] * np.exp(-alpha_t * -1)
+            weights = weights / np.sum(weights)  # normalize to add weights to 1
+            self.__stumps.append(tree)
 
-  def ginni_index(self, *args: float) -> float:
-    calculated_ginni: float = 1
-    for arg in args:
-      calculated_ginni -= arg ** 2
-    return calculated_ginni
+    def predict(self, data: pd.DataFrame, row_num: int) -> typing.Any:
+        """
+        Predict using the largest alpha values of sum for each unique prediction
+        :param data: data to make prediction from
+        :param row_num: which observation to make a prediction of
+        :return: prediction
+        """
+        predictions: dict[typing.Any, float] = {}
+        for stump, alpha in zip(self.__stumps, self.__alpha):
+            prediction: typing.Any = stump.predict(data, row_num)
+            if not prediction in predictions:
+                predictions[prediction] = alpha
+            else:
+                predictions[prediction] += alpha
 
-  def majority_error(self, data: pd.DataFrame, label: str) -> float:
-    target_series: pd.Series = data[label]
-    majority_label: typing.Any = target_series.mode()[0]
-    value_count: pd.Series = target_series.value_counts()
-    error: float = (target_series.shape[0] - target_series[target_series == majority_label].shape[0]) / sum(value_count)
-    return error
-
-  def __predict_single(self, data: pd.DataFrame, row_num: int) -> typing.Any:
-    '''
-    Only works with multiple observations
-    For one tree only
-    '''
-    current_node: Node = self.__head
-    while current_node.has_children():
-      # the value of the node determines what attribute to look down
-      value_node: typing.Any = current_node.get_value()
-      value_data_at_node: typing.Any = data.iloc[row_num][value_node]
-      current_node = current_node.get_child_edge(value_data_at_node)
-    return current_node.get_value()
-
-  def entropy(self, *args: float) -> float:
-    """
-    Calculate and return entropy based on the proportion data observed
-    """
-    calculated_entropy: float = 0
-    for arg in args:
-      calculated_entropy -= arg * math.log2(arg)
-    return calculated_entropy
-
-  def predict(self, data: pd.DataFrame, row_num: int) -> typing.Any:
-    predictions: list[typing.Any] = []
-    for tree in self.__stumps:
-      predictions.append(tree.__predict_single(data, row_num))
-    return np.sign(np.dot(self.__alpha, predictions))
+        # Find the key with the maximum value in the dictionary
+        return max(predictions, key=lambda k: predictions[k])
--- Ensemble Learning/test_AdaBoost.py
+++ Ensemble Learning/test_AdaBoost.py
@@ -12,9 +12,9 @@ class TestAdaBoost(TestCase):
     df["w"] = pd.Series(["w", "s", "w", "w", "w", "s", "s", "w", "w", "w", "s", "s", "w", "s"])
     df["play?"] = pd.Series([-1, -1, 1, 1, 1, -1, 1, -1, 1, 1, 1, 1, 1, -1])
     dtree: AdaBoost = AdaBoost(df)
-    dtree.build_boost( {"o", "t", "h", "w"}, "play?", dtree.entropy, 20)
+    dtree.build( {"o", "t", "h", "w"}, "play?", dtree.ENTROPY, 20, 1)
 
-  def test_build_boost_2(self):
+  def test_build_boost_ginni_index(self):
     df: pd.DataFrame = pd.DataFrame()
     df["o"] = pd.Series(["s", "s", "o", "r", "r", "r", "o", "s", "s", "r", "s", "o", "o", "r"])
     df["t"] = pd.Series(["h", "h", "h", "m", "c", "c", "c", "m", "c", "m", "m", "m", "h", "m"])
@@ -22,25 +22,39 @@ class TestAdaBoost(TestCase):
     df["w"] = pd.Series(["w", "s", "w", "w", "w", "s", "s", "w", "w", "w", "s", "s", "w", "s"])
     df["play?"] = pd.Series([-1, -1, 1, 1, 1, -1, 1, -1, 1, 1, 1, 1, 1, -1])
     dtree: AdaBoost = AdaBoost(df)
-    dtree.build_boost( {"o", "t", "h", "w"}, "play?", dtree.entropy, 20)
+    dtree.build( {"o", "t", "h", "w"}, "play?", dtree.GINNI_INDEX, 20, 1)
 
-  # def test_data_depth(self):
-  #   bank_train: pd.DataFrame = pd.read_csv("/Users/divytripathy/PycharmProjects/Machine Learning/DecisionTreePackage/bank/train.csv",
-  #                                          names=["age", "job", "marital", "education", "default", "balance", "housing",
-  #                                                 "loan", "contact", "day", "month", "duration", "campaign", "pdays",
-  #                                                 "previous", "poutcome", "y"])
-  #   bank_test: pd.DataFrame = pd.read_csv("/Users/divytripathy/PycharmProjects/Machine Learning/DecisionTreePackage/bank/test.csv",
-  #                                         names=["age", "job", "marital", "education", "default", "balance", "housing",
-  #                                                "loan", "contact", "day", "month", "duration", "campaign", "pdays",
-  #                                                "previous", "poutcome", "y"])
-  #   tree: AdaBoost = AdaBoost(bank_train)
-  #   tree.build_boost(attributes=set(bank_train.columns) - {"y"}, label="y", splitting_criteria=tree.entropy, num_trees=2)
-  #
-  #   def predict_dataframe(model: AdaBoost, test_data: pd.DataFrame, label: str) -> float:
-  #     accuracy_count: int = 0  # represents the count of correct predictions
-  #     for j in range(test_data.shape[0]):
-  #       if model.predict(test_data, j) == test_data[label].iloc[j]:
-  #         accuracy_count += 1
-  #     return accuracy_count / len(test_data)
-  #
-  #   print(predict_dataframe(tree, bank_test, "label"))
+  def test_predict(self):
+    bank_train: pd.DataFrame = pd.read_csv(
+      "/Users/divytripathy/PycharmProjects/Machine Learning/DecisionTreePackage/bank/train.csv",
+      names=["age", "job", "marital", "education", "default", "balance", "housing",
+             "loan", "contact", "day", "month", "duration", "campaign", "pdays",
+             "previous", "poutcome", "y"])
+    bank_test: pd.DataFrame = pd.read_csv(
+      "/Users/divytripathy/PycharmProjects/Machine Learning/DecisionTreePackage/bank/test.csv",
+      names=["age", "job", "marital", "education", "default", "balance", "housing",
+             "loan", "contact", "day", "month", "duration", "campaign", "pdays",
+             "previous", "poutcome", "y"])
+    tree: AdaBoost = AdaBoost(bank_train)
+
+    def make_numerical_attributes_binary(data):
+      column_types: dict[str, str] = data.dtypes.to_dict()
+      for column in column_types:
+        if column_types[column] == "int64" or column_types[column] == "float64":
+          data[column] = data[column].apply(lambda x: x >= data[column].median())
+
+    make_numerical_attributes_binary(bank_test)
+    make_numerical_attributes_binary(bank_train)
+
+    tree.build(attributes=set(bank_train.columns) - {"y"}, label="y", splitting_criteria=tree.ENTROPY,
+               num_trees=10, depth=5)
+
+    def predict_dataframe(model: AdaBoost, test_data: pd.DataFrame, label: str) -> float:
+      accuracy_count: int = 0  # represents the count of correct predictions
+      for j in range(test_data.shape[0]):
+        if model.predict(test_data, j) == test_data[label].iloc[j]:
+          accuracy_count += 1
+      return accuracy_count / len(test_data)
+
+    print(predict_dataframe(tree, bank_test, "y"))
+    print(predict_dataframe(tree, bank_train, "y"))
